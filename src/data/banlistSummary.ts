00b9ac63e6220a7f0827aa13223266ae12e1dd7f
André <veanve@gmail.com>
2026-01-08 22:00:48 -0300

    feat: update banlist JSON files with new lastUpdated date and card status changes

--- src/data/banlistSummary.ts
+++ src/data/banlistSummary.ts
@@ -9,21 +9,23 @@ export interface FormatSummary {
 // Update this object each month with the changes for each format
 export const banlistSummaries: Record<BanListFormat, FormatSummary[]> = {
   [BanListFormat.PRIMER_BLOQUE_LIBRE]: [
-    //{ card: 'Ejemplo Carta 1', pastMonth: 'Limitada x1', currentMonth: 'Baneada' },
+    { card: 'Festín', pastMonth: 'Baneada', currentMonth: 'Limitada x1' },
+    { card: 'Amón', pastMonth: 'Baneada', currentMonth: 'Limitada x1' },
+    { card: 'Cathbadh el Druida', pastMonth: 'Baneada', currentMonth: 'Limitada x1' },
+    { card: 'Averix el Sabio', pastMonth: 'Limitada x2', currentMonth: 'Liberado' },
+    { card: 'Kobold', pastMonth: 'Limitada x2', currentMonth: 'Limitada x1' },
+    { card: 'Ptolomeo II', pastMonth: 'Liberado', currentMonth: 'Limitada x2' },
+    { card: 'Daga de Bote', pastMonth: 'Liberado', currentMonth: 'Limitada x1' },
   ],
   [BanListFormat.PRIMER_BLOQUE_EDICION]: [
-    { card: 'Goblin Stone', pastMonth: 'Limitada x2', currentMonth: 'Liberado' },
-    { card: 'La Iliada', pastMonth: 'Limitada x1', currentMonth: 'Limitada x2' },
-    { card: 'Ofrenda de los Dioses', pastMonth: 'Limitada x1', currentMonth: 'Limitada x2' },
-    { card: 'Constantino', pastMonth: 'Limitada x2', currentMonth: 'Liberado' },
-    { card: 'Conand', pastMonth: 'Limitada x2', currentMonth: 'Liberado' },
-    { card: 'Linaje Celta', pastMonth: 'Limitada x2', currentMonth: 'Liberado' },
-    { card: 'Jeroglíficos', pastMonth: 'Liberado', currentMonth: 'Limitada x1' },
+    { card: 'Marco Antonio', pastMonth: 'Baneada', currentMonth: 'Liberado' },
+    { card: 'Rea', pastMonth: 'Limitada x2', currentMonth: 'Liberado' },
+    { card: 'Ptolomeo II', pastMonth: 'Limitada x2', currentMonth: 'Limitada x1' },
+    { card: 'Furia Irracional', pastMonth: 'Liberado', currentMonth: 'Limitada x1' },
+    { card: 'La Llama Fría', pastMonth: 'Liberado', currentMonth: 'Limitada x2' },
   ],
   [BanListFormat.BLOQUE_FURIA_LIBRE]: [
-    { card: 'Dragón Fomoriano', pastMonth: 'Liberado', currentMonth: 'Baneada' },
-    { card: 'Takam', pastMonth: 'Liberado', currentMonth: 'Limitada x1' },
-    { card: 'Jormundgander', pastMonth: 'Limitada x2', currentMonth: 'Liberado' },
+    //{ card: 'Dragón Fomoriano', pastMonth: 'Liberado', currentMonth: 'Baneada' },
   ],
   [BanListFormat.BLOQUE_FURIA_LIMITED]: [
     //{ card: 'Ejemplo Carta 6', pastMonth: 'Limitada x1', currentMonth: 'Liberado' },
@@ -31,4 +33,4 @@ export const banlistSummaries: Record<BanListFormat, FormatSummary[]> = {
 };
 
 // This will be displayed in the accordion title
-export const lastUpdateMonth = 'Diciembre 2025';
+export const lastUpdateMonth = 'Enero 2025';
