d9eae3fa646f8b75a97041d1e96daf82718c29f5
André <veanve@gmail.com>
2026-04-06 23:40:51 -0400

    fix: update lastUpdated dates and modify banlist entries across multiple formats

--- src/data/banlistSummary.ts
+++ src/data/banlistSummary.ts
@@ -13,17 +13,36 @@ export interface FormatSummary {
 // Update this object each month with the changes for each format
 export const banlistSummaries: Record<BanListFormat, FormatSummary[]> = {
   [BanListFormat.PRIMER_BLOQUE_LIBRE]: [
-    //{ card: 'Festín', pastMonth: 'Baneada', currentMonth: 'Limitada x1', changeType: 'positive' },
+    { card: 'País sin Luz', pastMonth: 'Limitada x1', currentMonth: 'Liberado', changeType: 'positive', imageUrl: 'https://cdn.mazos.cl/PRIMER_BLOQUE/20-AniversarioHD/cartas/026-AHD-Pais-sin-Luz-MazosCL.png'},
+    { card: 'Gruagash', pastMonth: 'Limitada x1', currentMonth: 'Liberado', changeType: 'positive', imageUrl: 'https://cdn.mazos.cl/PRIMER_BLOQUE/12-KitExtension/cartas/051-Gruagash.png'},
+    { card: 'Monte Kilgharrah', pastMonth: 'Limitada x2', currentMonth: 'Liberado', changeType: 'positive', imageUrl: 'https://cdn.mazos.cl/PRIMER_BLOQUE/12-AniversarioES/cartas/023-AESX-Monte-Kilgharrah-MazosCL.png'},
+    { card: 'Panteón', pastMonth: 'Limitada x2', currentMonth: 'Liberado', changeType: 'positive', imageUrl: 'https://cdn.mazos.cl/PRIMER_BLOQUE/15-AniversarioHE/cartas/026-AHE-Pantein-MazosCL.png'},
+    { card: 'Mercaderes', pastMonth: 'Limitada x1', currentMonth: 'Liberado', changeType: 'positive', imageUrl: 'https://cdn.mazos.cl/PRIMER_BLOQUE/01-Espada-Sagrada/cartas/Mercaderes-real.png-1764036016311'},
+    { card: 'Eolo', pastMonth: 'Baneada', currentMonth: 'Limitada x1', changeType: 'neutral', imageUrl: 'https://cdn.mazos.cl/PRIMER_BLOQUE/15-AniversarioHE/cartas/030-AHE-Eolo-MazosCL.png'},
+    { card: 'Felipe II', pastMonth: 'Baneada', currentMonth: 'Limitada x1', changeType: 'neutral', imageUrl: 'https://cdn.mazos.cl/PRIMER_BLOQUE/01-Espada-Sagrada/cartas/Felipe-ii-real.png-1764035983821'},
+    { card: 'Marco Antonio', pastMonth: 'Baneada', currentMonth: 'Limitada x1', changeType: 'neutral', imageUrl: 'https://cdn.mazos.cl/PRIMER_BLOQUE/15-AniversarioHE/cartas/017-AHE-Marco-Antonio-MazosCL.png'},
+    { card: 'Ptolomeo II', pastMonth: 'Limitada x2', currentMonth: 'Limitada x1', changeType: 'neutral', imageUrl: 'https://cdn.mazos.cl/PRIMER_BLOQUE/25-AniversarioRA/cartas/083-ARA-Ptolomeo-II-MazosCL.png'},
+    { card: 'Abu Simbel', pastMonth: 'Liberado', currentMonth: 'Limitada x1', changeType: 'neutral', imageUrl: 'https://cdn.mazos.cl/PRIMER_BLOQUE/29-Toolkit2026/cartas/028-TKPB26-Abu-Simbel-MazosCL.webp'},
+    { card: 'Traficantes de Esclavos', pastMonth: 'Liberado', currentMonth: 'Baneada', changeType: 'negative', imageUrl: 'https://cdn.mazos.cl/PRIMER_BLOQUE/19-Leyendas3PB/cartas/200-LPB23-Traficantes-MazosCL.png'},
+    { card: 'Urisk', pastMonth: 'Liberado', currentMonth: 'Baneada', changeType: 'negative', imageUrl: 'https://cdn.mazos.cl/PRIMER_BLOQUE/19-Leyendas3PB/cartas/199-LPB23-Urisk-MazosCL.png'},
+    { card: 'Yelmo Alejandrino', pastMonth: 'Liberado', currentMonth: 'Baneada', changeType: 'negative', imageUrl: 'https://cdn.mazos.cl/PRIMER_BLOQUE/25-AniversarioRA/cartas/161-ARA-Yelmo-Alejandrino-MazosCL.png'},
   ],
   [BanListFormat.PRIMER_BLOQUE_EDICION]: [
-    { card: 'Fergus', pastMonth: 'Baneada', currentMonth: 'Limitada x1', changeType: 'positive', imageUrl: 'https://cdn.mazos.cl/PRIMER_BLOQUE/19-Leyendas3PB/cartas/046-LPB23-Fergus-MazosCL.png'},
-    { card: 'Ofrenda a los Dioses', pastMonth: 'Limitada x2', currentMonth: 'Liberado', changeType: 'positive', imageUrl: 'https://cdn.mazos.cl/PRIMER_BLOQUE/19-Leyendas3PB/cartas/146-LPB23-Ofrenda-a-los-Dioses-MazosCL.png' },
-    { card: 'La llama Fría', pastMonth: 'Limitada x2', currentMonth: 'Limitada x1', changeType: 'neutral', imageUrl: 'https://cdn.mazos.cl/PRIMER_BLOQUE/28-Lootbox25/cartas/028-LBPB25-La-Llama-fria-MazosCL.png' },
-    { card: 'Tebas', pastMonth: 'Limitada x2', currentMonth: 'Limitada x1', changeType: 'neutral', imageUrl: 'https://cdn.mazos.cl/PRIMER_BLOQUE/25-AniversarioRA/cartas/270-ARA-Tebas-MazosCL.png' },
-    { card: 'Kamose el Guerrero', pastMonth: 'Liberado', currentMonth: 'Limitada x2', changeType: 'neutral', imageUrl: 'https://cdn.mazos.cl/PRIMER_BLOQUE/25-AniversarioRA/cartas/295-ARA-Kamose-el-Guerrero-MazosCL.png' },
+    { card: 'Morir de Pie', pastMonth: 'Limitada x1', currentMonth: 'Limitada x2', changeType: 'neutral', imageUrl: 'https://cdn.mazos.cl/PRIMER_BLOQUE/20-AniversarioHD/relatos/007-AHDX-Morir-de-Pie-MazosCL.png'},
+    { card: 'Helios', pastMonth: 'Limitada x1', currentMonth: 'Limitada x2', changeType: 'neutral', imageUrl: 'https://cdn.mazos.cl/PRIMER_BLOQUE/22-Lootbox24/cartas/039-LB24-Helios-MazosCL.png'},
+    { card: 'País sin Luz', pastMonth: 'Limitada x2', currentMonth: 'Liberado', changeType: 'positive', imageUrl: 'https://cdn.mazos.cl/PRIMER_BLOQUE/20-AniversarioHD/cartas/026-AHD-Pais-sin-Luz-MazosCL.png'},
+    { card: 'Avalon', pastMonth: 'Baneada', currentMonth: 'Limitada x1', changeType: 'neutral', imageUrl: 'https://cdn.mazos.cl/PRIMER_BLOQUE/17-Lootbox3/cartas/054-Avalon-MazosCL.png'},
+    { card: 'Mineros de Lapislázuli', pastMonth: 'Liberado', currentMonth: 'Limitada x2', changeType: 'neutral', imageUrl: 'https://cdn.mazos.cl/PRIMER_BLOQUE/25-AniversarioRA/cartas/385-ARA-Mineros-de-Lapislazuli-MazosCL.png'},
+    { card: 'Kernuac el Cazador', pastMonth: 'Liberado', currentMonth: 'Baneada', changeType: 'negative', imageUrl: 'https://cdn.mazos.cl/PRIMER_BLOQUE/12-AniversarioES/cartas/074-Kernuac-el-Cazador-MazosCL.png'},
   ],
   [BanListFormat.BLOQUE_FURIA_LIBRE]: [
-    //{ card: 'Dragón Fomoriano', pastMonth: 'Liberado', currentMonth: 'Baneada' },
+    { card: 'Enki', pastMonth: 'Liberado', currentMonth: 'Baneada', changeType: 'negative', imageUrl: 'https://cdn.mazos.cl/FURIA_EXT/10-Leyendas2/numeradas/316.webp'},
+    { card: 'Corte Terrenal', pastMonth: 'Liberado', currentMonth: 'Baneada', changeType: 'negative', imageUrl: 'https://cdn.mazos.cl/FURIA_EXT/10-Leyendas2/numeradas/307.webp'},
+    { card: 'Tribunal Eterno', pastMonth: 'Liberado', currentMonth: 'Limitada x1', changeType: 'neutral', imageUrl: 'https://cdn.mazos.cl/FURIA_EXT/10-Leyendas2/numeradas/315.webp'},
+    { card: 'Humbaba', pastMonth: 'Baneada', currentMonth: 'Limitada x1', changeType: 'neutral', imageUrl: 'https://cdn.mazos.cl/FURIA_EXT/06-Daana/promos/181-GUA-Humbaba-MazosCL.webp'},
+    { card: 'Dragón Fomoriano', pastMonth: 'Baneada', currentMonth: 'Limitada x1', changeType: 'neutral', imageUrl: 'https://cdn.mazos.cl/FURIA_EXT/06-Daana/promos/175-GUA-Dragon-Fomoriano-MazosCL.webp'},
+    { card: 'Cangrejo Cazador', pastMonth: 'Limitada x1', currentMonth: 'Limitada x2', changeType: 'neutral', imageUrl: 'https://cdn.mazos.cl/FURIA_EXT/06-Daana/cartas/037-GUA-Cangrejo-Cazador-MazosCL.webp'},
+    { card: 'Takam', pastMonth: 'Limitada x1', currentMonth: 'Limitada x2', changeType: 'neutral', imageUrl: 'https://cdn.mazos.cl/FURIA_EXT/10-Leyendas2/cartas-nombre/104-LBF2-Takam-MazosCL.webp'},
   ],
   [BanListFormat.BLOQUE_FURIA_LIMITED]: [
     //{ card: 'Ejemplo Carta 6', pastMonth: 'Limitada x1', currentMonth: 'Liberado' },
@@ -31,4 +50,4 @@ export const banlistSummaries: Record<BanListFormat, FormatSummary[]> = {
 };
 
 // This will be displayed in the accordion title
-export const lastUpdateMonth = 'Marzo 2026';
+export const lastUpdateMonth = 'Abril 2026';
